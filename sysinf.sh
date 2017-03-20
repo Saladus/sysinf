3b0f7f080da03b66bafbfa3dd7b4e8104f0228fb
Saladus <saladus@cryptolab.net>
2017-03-20 23:27:46 +0200

    Add files via upload

---
+++ sysinf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+kasutaja=`whoami`
+raal=`uname -n`
+os=`head -1 /etc/os-release | grep -o -P '(?<=").*(?=")'`
+kernel=`uname -m && uname -r`
+viimane_boot=`uptime -s`
+
+if [[ $os == Arch\ Linux ]]
+	then pakette=`pacman  -Q | wc -l`
+elif [[ $os == Ubuntu ]] 
+	then pakette=`apt-cache pkgnames | wc -l`
+fi
+
+ul=`tput smul`
+rmul=`tput rmul`
+b=`tput bold`
+sinine=`tput setaf 6`
+ilma=`tput sgr0`
+
+echo ${vi}${ul}${b}${sinine}$kasutaja${ilma}${ul}"@"${b}${sinine}$raal${ilma}
+echo ${b}${sinine}"OS:"${ilma} $os
+echo ${b}${sinine}"Kernel:"${ilma} $kernel
+echo ${b}${sinine}"Tarkvara:"${ilma} $pakette
+echo ${b}${sinine}"Ärkvel alates:"${ilma} $viimane_boot
